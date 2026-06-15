f121122adfbdcbbad92f8b7ae51e2c2658c3d45a
Juan Sommacal <juanpsommacal@gmail.com>
2026-06-15 12:08:46 -0300

    Agrego validacion

--- src/main/java/SSVG/TPFinalDeliveryMascotero/Model/DTO/Request/Pago/PagoCreateRequestDTO.java
+++ src/main/java/SSVG/TPFinalDeliveryMascotero/Model/DTO/Request/Pago/PagoCreateRequestDTO.java
@@ -1,12 +1,9 @@
 package SSVG.TPFinalDeliveryMascotero.Model.DTO.Request.Pago;
 
 import SSVG.TPFinalDeliveryMascotero.Model.Enums.MetodoPago;
-import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.*;
 import SSVG.TPFinalDeliveryMascotero.Model.Enums.UnidadMedida;
 import SSVG.TPFinalDeliveryMascotero.Validations.ValidEnum;
-import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,6 +27,7 @@ public class PagoCreateRequestDTO {
 
     @NotNull(message = "El metodo de pago no puede estar vacio")
     @ValidEnum(enumClass = MetodoPago.class, message = "El metodo de pago debe ser EFECTIVO, LINK_DE_PAGO, TRASNFERENCIA")
+    @Size(max = 30, message = "El metodo de pago no puede exceder los 30 caracteres")
     private String metodoPago;
 
 }
